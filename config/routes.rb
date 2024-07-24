f917171220769a3b6398f86b96e2e5bd5cdc78da
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-07-24 15:07:53 -0600

    Resolved merge conflict in Gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'invoices/index'
+    get 'invoices/show'
+    get 'merchants/index'
+    get 'merchants/show'
+    get 'merchants/edit'
+    get '/', to: 'dashboard#index', as: 'dashboard'
+    get '/merchants', to: 'merchants#index', as: 'merchants'
+    get '/invoices', to: 'invoices#index', as: 'invoices'
+    
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

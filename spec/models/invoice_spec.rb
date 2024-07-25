c1bb17f12745adce348d6ad91a72c8f8f362e29b
Lance Nelson <lancen04@gmail.com>
2024-07-25 17:45:53 -0400

    added model test for customer_full_name to Invoice model spec

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe Invoice, type: :model do
 	describe "validations" do
 		it { should define_enum_for(:status).with_values([:"in progress", :completed, :cancelled]) }
 	end
+
+	before(:each) do
+		@customer = create(:customer)
+		@customer2 = create(:customer)
+		@invoice1 = create(:invoice, customer: @customer)
+		@invoice2 = create(:invoice, customer: @customer)
+		@invoice3 = create(:invoice, customer: @customer2)
+	end
+
+	describe "instance_methods" do
+		it "customer_full_name" do
+			expect(@invoice1.customer_full_name).to eq(@customer.first_name + " " + @customer.last_name)
+			expect(@invoice2.customer_full_name).to eq(@customer.first_name + " " + @customer.last_name)
+			expect(@invoice3.customer_full_name).to eq(@customer2.first_name + " " + @customer2.last_name)
+		end
+	end
 end

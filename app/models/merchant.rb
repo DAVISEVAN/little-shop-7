fe6eb85dfe39f25d65f1eac3570a7c50ab11170c
Tyler Noble <tmnoble11@gmail.com>
2024-07-25 16:23:01 -0600

    query in merchant model finished. Merchant_spec for US3 passing

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,7 +6,7 @@ class Merchant < ApplicationRecord
     Customer
       .joins(invoices: :transactions)
       .where(invoices: { id: self.invoices.pluck(:id) }, transactions: { result: 0 })  # 0 corresponds to 'success'
-      .select('customers.*, COUNT(transactions.id) AS transaction_count')
+      .select('customers.*, COUNT(transactions.id) as transaction_count')
       .group('customers.id')
       .order('transaction_count DESC')
       .limit(5)

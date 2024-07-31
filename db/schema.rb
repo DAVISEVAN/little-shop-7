a87524f1a4a8aaae818ba59515a804aa7c68f7c9
Lance Nelson <lancen04@gmail.com>
2024-07-31 17:47:46 -0400

    edited merchant migration to set default status to disabled

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_31_204508) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status", default: 0
+    t.integer "status", default: 1
   end
 
   create_table "transactions", force: :cascade do |t|

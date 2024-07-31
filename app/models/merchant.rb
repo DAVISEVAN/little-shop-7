6f68a6307edcc147ac2cef73ea17ae7d59fbca79
Austin K <austink@Austins-MacBook-Pro.local>
2024-07-31 19:13:43 -0400

    added model method best_day

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,9 @@ class Merchant < ApplicationRecord
   has_many :invoices, through: :items
 
   enum status: { enabled: 0, disabled: 1 }
+
+  def best_day
+    return "" if invoices.blank?
+    invoices.max_by{|invoice| invoice.total_revenue}.created_at
+  end
 end
